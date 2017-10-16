6412fb3fb014284f81e38ae33a21970ed97c0783
Ben Eyal <bene@post.bgu.ac.il>
2017-10-16 13:46:21 +0300

    Parse trees now show as expected

--- static/viz.js
+++ static/viz.js
@@ -4,9 +4,7 @@
 
 const getArrowId = (() => {
     let n = 1;
-    return () => {
-        return n++;
-    }
+    return () => n++;
 })();
 
 // Size parameters
@@ -36,63 +34,47 @@ const svg = d3
     .select('body')
     .append('svg')
     .attr('width', width)
-    .attr('height', height);
+    .attr('height', 2 * height);
 
 /****************
  * Render Words *
  ****************/
 
-// Setup English area
-const englishWordsArea = svg
-    .selectAll('#english')
-    .data(data)
-    .enter()
-    .append('text')
-    .classed('english', true)
-    .attr('text-anchor', 'middle')
-    .attr('y', enOffsetY + wordSpacing);
+const renderWords = (lang, y) => {
+    // Setup words area
+    const wordsArea = svg
+        .selectAll(`#${lang}`)
+        .data(data)
+        .enter()
+        .append('text')
+        .classed('english', true)
+        .attr('text-anchor', 'middle')
+        .attr('y', y);
 
-/// Add words
-englishWordsArea
-    .append('tspan')
-    .attr('x', (d, i) => offsetX + i * distance)
-    .text(d => d.text);
+    /// Add words
+    wordsArea
+        .append('tspan')
+        .attr('x', (d, i) => offsetX + i * distance)
+        .text(d => d.text);
 
-/// Add tags 2em below words
-englishWordsArea
-    .append('tspan')
-    .attr('x', (d, i) => offsetX + i * distance)
-    .attr('dy', '2em')
-    .text(d => d.tag);
+    /// Add tags 2em below words
+    wordsArea
+        .append('tspan')
+        .attr('x', (d, i) => offsetX + i * distance)
+        .attr('dy', '2em')
+        .text(d => d.tag);
+};
 
-// Setup Hebrew area
-const hebrewWordsArea = svg
-    .selectAll('#hebrew')
-    .data(data)
-    .enter()
-    .append('text')
-    .classed('hebrew', true)
-    .attr('text-anchor', 'middle')
-    .attr('y', height - alignmentSpace);
-
-/// Add tags
-hebrewWordsArea
-    .append('tspan')
-    .attr('x', (d, i) => offsetX + i * distance)
-    .text(d => d.tag);
-
-/// Add words 2em below words
-hebrewWordsArea
-    .append('tspan')
-    .attr('x', (d, i) => offsetX + i * distance)
-    .attr('dy', '2em')
-    .text(d => d.text);
+renderWords('english', enOffsetY + wordSpacing);
+renderWords('hebrew', enOffsetY + alignmentSpace);
 
 /*****************
  * Render Arrows *
  *****************/
 
-const renderEnglishArrow = ({ head, deprel }, i, nodes) => {
+const make_renderer = (lang, highestLevel, offsetY) => function ({ head, deprel }, i, nodes) {
+    const direction = lang.toLowerCase() === 'english' ? -1 : 1;
+
     if (deprel === 'ROOT') {
         return;
     }
@@ -101,14 +83,9 @@ const renderEnglishArrow = ({ head, deprel }, i, nodes) => {
     const level = Math.abs(head - i);
     const start = head < i ? head : i;
     const dir = head > i ? 'left' : 'right';
-    const startX = offsetX + start * distance + arrowSpacing * (enHighestLevel - level) / 4;
-    const startY = enOffsetY;
-    const endpoint = offsetX + level * distance + start * distance - arrowSpacing * (enHighestLevel - level) / 4;
-
-    let curve = enOffsetY - level * distance / 2;
-    if (curve === 0 && enLevels.length > 5) {
-        curve = -distance;
-    }
+    const startX = offsetX + start * distance + arrowSpacing * (highestLevel - level) / 4;
+    const startY = offsetY;
+    const endpoint = offsetX + level * distance + start * distance - arrowSpacing * (highestLevel - level) / 4;
 
     const group = d3.select(nodes[i]);
 
@@ -116,15 +93,15 @@ const renderEnglishArrow = ({ head, deprel }, i, nodes) => {
         .append('path')
         .attr('id', 'arrow-' + arrowId)
         .attr('d', `M${startX},${startY}
-                    C${startX},${curve}
-                     ${endpoint},${curve}
+                    L${startX + 10},${startY + direction * level * 50}
+                     ${endpoint - 10},${startY + direction * level * 50}
                      ${endpoint},${startY}`)
         .attr('fill', 'none')
         .attr('stroke', 'black');
 
     group
         .append('text')
-        .attr('dy', '1em')
+        .attr('dy', `${-direction}em`)
         .append('textPath')
         .attr('xlink:href', '#arrow-' + arrowId)
         .attr('text-anchor', 'middle')
@@ -134,78 +111,31 @@ const renderEnglishArrow = ({ head, deprel }, i, nodes) => {
 
     group
         .append('path')
-        .attr('d', `M${(dir === 'left') ? startX : endpoint},${startY + 2}
-                    L${(dir === 'left') ? startX - arrowWidth + 2 : endpoint + arrowWidth - 2},${startY - arrowWidth}
-                     ${(dir === 'left') ? startX + arrowWidth - 2 : endpoint - arrowWidth + 2},${startY - arrowWidth}
+        .attr('d', `M${(dir === 'left') ? startX : endpoint},${startY - direction * 2}
+                    L${(dir === 'left') ? startX - arrowWidth + 2 : endpoint + arrowWidth - 2},${startY + direction * arrowWidth}
+                     ${(dir === 'left') ? startX + arrowWidth - 2 : endpoint - arrowWidth + 2},${startY + direction * arrowWidth}
                     Z`)
         .attr('stroke', 'black');
 };
 
-const englishArrowsArea = svg
+svg
     .selectAll('#enArrow')
     .data(data)
     .enter()
     .append('g')
     .classed('enArrow', true)
-    .each(renderEnglishArrow);
+    .each(make_renderer('english', enHighestLevel, enOffsetY));
 
-/*****************
- * Render Arrows *
- *****************/
-
-const renderHebrewArrow = ({ head, deprel }, i, nodes) => {
-    if (deprel === 'ROOT') {
-        return;
-    }
-
-    const arrowId = getArrowId();
-    const level = Math.abs(head - i);
-    const start = head < i ? head : i;
-    const dir = head > i ? 'left' : 'right';
-    const startX = offsetX + start * distance + arrowSpacing * (heHighestLevel - level) / 4;
-    const startY = heOffsetY;
-    const endpoint = offsetX + level * distance + start * distance - arrowSpacing * (heHighestLevel - level) / 4;
-
-    let curve = heOffsetY + wordSpacing + level * distance / 2;
-    if (curve === 0 && heLevels.length > 5) {
-        curve = -distance;
-    }
-
-    const group = d3.select(nodes[i]);
-
-    group
-        .append('path')
-        .attr('id', 'arrow-' + arrowId)
-        .attr('d', `M${startX},${startY}
-                    C${startX},${curve}
-                     ${endpoint},${curve}
-                     ${endpoint},${startY}`)
-        .attr('fill', 'none')
-        .attr('stroke', 'black');
-
-    group
-        .append('text')
-        .attr('dy', '1em')
-        .append('textPath')
-        .attr('xlink:href', '#arrow-' + arrowId)
-        .attr('text-anchor', 'middle')
-        .attr('fill', 'black')
-        .attr('startOffset', '50%')
-        .text(deprel);
-
-    group
-        .append('path')
-        .attr('d', `M${(dir === 'left') ? startX : endpoint},${startY - 2}
-                    L${(dir === 'left') ? startX - arrowWidth + 2 : endpoint + arrowWidth - 2},${startY + arrowWidth}
-                     ${(dir === 'left') ? startX + arrowWidth - 2 : endpoint - arrowWidth + 2},${startY + arrowWidth}
-                    Z`)
-        .attr('stroke', 'black');
-};
-
-const hebrewArrowsArea = svg
+svg
     .selectAll('#heArrow')
     .data(data)
     .enter()
     .append('g')
     .classed('heArrow', true)
-    .each(renderHebrewArrow);
+    .each(make_renderer('hebrew', heHighestLevel, enOffsetY + alignmentSpace + 2 * wordSpacing));
+
+/********************
+ * Render Alignment *
+ ********************/
+
+//TODO
