f5dcc1ca9af6bee8f801271741459474d6b44377
Ben Eyal <bene@post.bgu.ac.il>
2017-10-21 12:07:46 +0300

    Add dummy data

---
+++ static/data.js
@@ -0,0 +1,62 @@
+const data = [
+    {
+        "text": "The",
+        "tag": "DT",
+        "head": 3,
+        "deprel": "det"
+    },
+    {
+        "text": "quick",
+        "tag": "JJ",
+        "head": 3,
+        "deprel": "amod"
+    },
+    {
+        "text": "brown",
+        "tag": "JJ",
+        "head": 3,
+        "deprel": "amod"
+    },
+    {
+        "text": "fox",
+        "tag": "NN",
+        "head": 4,
+        "deprel": "nsubj"
+    },
+    {
+        "text": "jumped",
+        "tag": "VBD",
+        "head": -1,
+        "deprel": "ROOT"
+    },
+    {
+        "text": "over",
+        "tag": "IN",
+        "head": 4,
+        "deprel": "prep"
+    },
+    {
+        "text": "the",
+        "tag": "DT",
+        "head": 8,
+        "deprel": "det"
+    },
+    {
+        "text": "lazy",
+        "tag": "JJ",
+        "head": 8,
+        "deprel": "amod"
+    },
+    {
+        "text": "dog",
+        "tag": "NN",
+        "head": 5,
+        "deprel": "pobj"
+    },
+    {
+        "text": ".",
+        "tag": ".",
+        "head": 4,
+        "deprel": "punct"
+    }
+]
