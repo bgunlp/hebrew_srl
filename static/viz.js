d02bddb97ce51019d7a0ba596cc7f15a63192886
Ben Eyal <bene@post.bgu.ac.il>
2017-10-22 01:48:21 +0300

    Replace POS-tags with SRL data.

--- static/viz.js
+++ static/viz.js
@@ -57,15 +57,52 @@ const renderWords = (data, lang, y) => {
         .text(d => d.form);
 
     /// Add tags 2em below words
-    wordsArea
-        .append('tspan')
-        .attr('x', (d, i) => offsetX + i * distance)
-        .attr('dy', '2em')
-        .text(d => d.upostag);
+    // wordsArea
+    //     .append('tspan')
+    //     .attr('x', (d, i) => offsetX + i * distance)
+    //     .attr('dy', '2em')
+    //     .text(d => d.upostag);
+
+    return wordsArea;
 };
 
 renderWords(data.english.words,'english', enOffsetY + wordSpacing);
 renderWords(data.hebrew.words, 'hebrew', enOffsetY + alignmentSpace);
+
+/**************
+ * Render SRL *
+ **************/
+
+data.english.frames.sort((a, b) => b.annotationSets[0].score - a.annotationSets[0].score);
+
+const { target, annotationSets } = data.english.frames[0];
+const { frameElements } = annotationSets[0];
+
+svg
+    .append('text')
+    .classed('english-srl-frame', true)
+    .attr('text-anchor', 'middle')
+    .attr('y', enOffsetY + wordSpacing)
+    .append('tspan')
+    .attr('x', offsetX + target.spans[0].start * distance)
+    .attr('dy', '1em')
+    .text(target.name)
+    .style('font-variant', 'small-caps');
+
+frameElements.forEach(({ name, spans }) => {
+    const { start, end } = spans[0];
+    for (let i = start; i < end; i++) {
+        svg
+            .append('text')
+            .classed('english-srl-fe', true)
+            .attr('text-anchor', 'middle')
+            .attr('y', enOffsetY + wordSpacing)
+            .append('tspan')
+            .attr('x', offsetX + i * distance)
+            .attr('dy', '2em')
+            .text(name);
+    }
+});
 
 /*****************
  * Render Arrows *
