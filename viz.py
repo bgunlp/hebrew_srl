2547c79b663a171101f2049c8877188de28c79fd
Ben Eyal <bene@post.bgu.ac.il>
2017-10-28 16:49:36 +0300

    Add persistence for annotations

--- viz.py
+++ viz.py
@@ -2,14 +2,18 @@ import json
 import os
 
 from conllu.parser import parse as conllu_parse
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_bootstrap import Bootstrap
+from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import FlaskForm
 from wtforms import BooleanField, RadioField, SubmitField
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'balls'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.sqlite3'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 Bootstrap(app)
+db = SQLAlchemy(app)
 
 DATA_ROOT = os.path.join(app.root_path, 'static', 'datasubset')
 
@@ -24,6 +28,16 @@ class AnnotationForm(FlaskForm):
         ('ok', 'OK')
     ])
     submit = SubmitField('Annotate')
+
+
+class Annotation(db.Model):
+    __tablename__ = 'annotations'
+    file = db.Column(db.String(40), primary_key=True)
+    sentence = db.Column(db.Integer, primary_key=True)
+    message = db.Column(db.String(20))
+
+    def __repr__(self):
+        return f'<Annotation for {self.file}/{self.sentence}>'
 
 
 def english_sents(filename):
@@ -74,20 +88,16 @@ def sentence_select(filename):
     return render_template('sentenceselect.html', filename=filename, sents=sents, page='Sentence Selection')
 
 
-@app.route('/<filename>/<sent_id>')
 @app.route('/<filename>/<sent_id>', methods=['GET', 'POST'])
 def tree_view(filename, sent_id):
     form = AnnotationForm()
+    if request.method == 'POST':
+        if Annotation.query.filter_by(file=filename, sentence=sent_id).first() is None:
+            annotation = Annotation(file=filename, sentence=sent_id, message=form.annotation.data)
+            db.session.add(annotation)
+            db.session.commit()
     data = create(filename)
-    return render_template('treeview.html', data=data[int(sent_id)], page='Graphic')
-
-# TODO Annotate
-# - Error in sentence alignment
-# - Error in word alignment
-# - Poor translation
-# - Poor syntactic parsing
-# - Poor frame parsing
-# - OK
+    return render_template('treeview.html', data=data[int(sent_id)], page='Graphic', form=form)
 
 
 if __name__ == '__main__':
