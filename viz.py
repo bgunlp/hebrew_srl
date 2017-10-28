b89eb43551a9534d2c31fdc5e573b3991bcfc2e1
Ben Eyal <bene@post.bgu.ac.il>
2017-10-28 16:48:39 +0300

    Add and use WTForms

--- viz.py
+++ viz.py
@@ -4,11 +4,26 @@ import os
 from conllu.parser import parse as conllu_parse
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm
+from wtforms import BooleanField, RadioField, SubmitField
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'balls'
 Bootstrap(app)
 
 DATA_ROOT = os.path.join(app.root_path, 'static', 'datasubset')
+
+
+class AnnotationForm(FlaskForm):
+    annotation = RadioField('Annotation', choices=[
+        ('err_sent', 'Error in Sentence Alignment'),
+        ('err_word', 'Error in Word Alignment'),
+        ('poor_trans', 'Poor Translation'),
+        ('poor_syn', 'Poor Syntactic Parsing'),
+        ('poor_frame', 'Poor Frame Parsing'),
+        ('ok', 'OK')
+    ])
+    submit = SubmitField('Annotate')
 
 
 def english_sents(filename):
@@ -60,7 +75,9 @@ def sentence_select(filename):
 
 
 @app.route('/<filename>/<sent_id>')
+@app.route('/<filename>/<sent_id>', methods=['GET', 'POST'])
 def tree_view(filename, sent_id):
+    form = AnnotationForm()
     data = create(filename)
     return render_template('treeview.html', data=data[int(sent_id)], page='Graphic')
 
