c23d121619758f4b0cd7c8e84b6de8eb8815e4cc
Ben Eyal <bene@post.bgu.ac.il>
2017-12-19 00:06:13 +0200

    Add parse tree depth feature

--- classifier.py
+++ classifier.py
@@ -9,14 +9,20 @@ from imblearn.over_sampling import RandomOverSampler
 from imblearn.pipeline import Pipeline
 from sklearn.feature_extraction import DictVectorizer
 from sklearn.linear_model import PassiveAggressiveClassifier
-# from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report
 
+Node = namedtuple('Node', 'token distance')
 
-def dfs(sentence, start):
-    visited, stack = set(), [start]
+
+def parse_tree_depth(sentence, root):
+    visited, stack = [], [root]
     while stack:
-        pass
+        node = stack.pop()
+        if node not in visited:
+            visited.append(node)
+            stack.extend([Node(t, node.distance + 1) for t in sentence if t['head'] == node.token['id']])
+    return max(visited, key=itemgetter(1)).distance
 
 
 def features(sentence):
@@ -31,13 +37,10 @@ def features(sentence):
     one_to_ones = len([g for g in alignment_groups if len(g) == 1])
     one_to_manys = len(alignment_groups) - one_to_ones
 
-    Node = namedtuple('Node', 'token distance')
-    en_head = [x for x in sentence['english']['words'] if x['head'] == 0],
-    he_head = [x for x in sentence['hebrew']['words'] if x['head'] == 0],
-    en_head_node = Node(en_head, 0)
-    he_head_node = Node(he_head, 0)
-
-
+    en_head, = [x for x in sentence['english']['words'] if x['head'] == 0]
+    he_head, = [x for x in sentence['hebrew']['words'] if x['head'] == 0]
+    en_parse_tree_depth = parse_tree_depth(sentence['english']['words'], Node(en_head, 0))
+    he_parse_tree_depth = parse_tree_depth(sentence['hebrew']['words'], Node(he_head, 0))
 
     return {
         'en-sent-length': en_sent_length,
@@ -45,17 +48,10 @@ def features(sentence):
         'en-he-ratio': en_sent_length / he_sent_length,
         'number-of-frames': nframes,
         '1-1s': one_to_ones / en_sent_length,
-        '1-ns': one_to_manys / en_sent_length
+        '1-ns': one_to_manys / en_sent_length,
+        'en-parse-tree-depth': en_parse_tree_depth,
+        'he-parse-tree-depth': he_parse_tree_depth
     }
-
-
-all_annotations = Annotation.query.all()
-cutoff = int(.75 * len(all_annotations))
-training_annotations = all_annotations[:cutoff]
-test_annotations = all_annotations[cutoff:]
-
-print(len(training_annotations))
-print(len(test_annotations))
 
 
 def transform_to_dataset(annotations):
@@ -69,7 +65,10 @@ def transform_to_dataset(annotations):
     return X, y
 
 
-X, y = transform_to_dataset(training_annotations)
+X_train, X_test, y_train, y_test = train_test_split(*transform_to_dataset(Annotation.query.all()), random_state=0)
+
+print(len(X_train))
+print(len(X_test))
 
 clf = Pipeline([
     ('vectorizer', DictVectorizer(sparse=True)),
@@ -77,11 +76,9 @@ clf = Pipeline([
     ('classifier', PassiveAggressiveClassifier(max_iter=50))
 ])
 
-clf.fit(X, y)
+clf.fit(X_train, y_train)
 
 print('Training Completed')
-
-X_test, y_test = transform_to_dataset(test_annotations)
 
 print("Accuracy:", clf.score(X_test, y_test))
 print(classification_report(y_test, clf.predict(X_test)))
